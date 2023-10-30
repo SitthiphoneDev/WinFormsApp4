3ef6ef424efb4a1b2c10aa81b3e7ee206bf3729d
Sitthiphone <108825240+SitthiphoneDev@users.noreply.github.com>
2023-10-30 13:16:02 +0700

    Add project files.

---
+++ WinFormsApp4/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace WinFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            button1 = new Button();
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            button2 = new Button();
+            button3 = new Button();
+            pictureBox2 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(602, 28);
+            button1.Name = "button1";
+            button1.Size = new Size(175, 50);
+            button1.TabIndex = 0;
+            button1.Text = "Set Pixel";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(34, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(274, 267);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            pictureBox1.MouseMove += pictureBox1_MouseMove;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(598, 107);
+            label1.Name = "label1";
+            label1.Size = new Size(21, 20);
+            label1.TabIndex = 2;
+            label1.Text = "R:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(598, 148);
+            label2.Name = "label2";
+            label2.Size = new Size(22, 20);
+            label2.TabIndex = 3;
+            label2.Text = "G:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(598, 188);
+            label3.Name = "label3";
+            label3.Size = new Size(21, 20);
+            label3.TabIndex = 4;
+            label3.Text = "B:";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(604, 227);
+            button2.Name = "button2";
+            button2.Size = new Size(152, 38);
+            button2.TabIndex = 5;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(601, 290);
+            button3.Name = "button3";
+            button3.Size = new Size(157, 29);
+            button3.TabIndex = 6;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(34, 285);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(274, 267);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 7;
+            pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 735);
+            Controls.Add(pictureBox2);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            MouseMove += Form1_MouseMove;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private PictureBox pictureBox1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button button2;
+        private Button button3;
+        private PictureBox pictureBox2;
+    }
+}
---
+++ WinFormsApp4/Form1.cs
@@ -0,0 +1,72 @@
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WinFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public Bitmap ConvertToGray(Bitmap source)
+        {
+            Bitmap bmp = new Bitmap(source.Width, source.Height);
+            for (int x = 0; x < bmp.Width; x++)
+            {
+                for (int y = 0; y < bmp.Height; y++)
+                {
+                    Color c = source.GetPixel(x, y);
+                    int avr = (int)((c.R + c.G + c.B) / 3);
+                    bmp.SetPixel(x, y, Color.FromArgb(avr, avr, avr));
+                }
+            }
+            return bmp;
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Bitmap srcBitmap = new Bitmap(pictureBox1.Image);
+            for (int x = 10; x < 120; x++)
+            {
+                //  srcBitmap.SetPixel(x, 10, Color.Red);
+                srcBitmap.SetPixel(x, 10, Color.Red);
+            }
+            pictureBox1.Image = srcBitmap;
+
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Bitmap bmp = (Bitmap)pictureBox1.Image;
+            Color clr = bmp.GetPixel(e.X, e.Y);
+            //convert number to string by ToString()
+            label1.Text = "R: " + clr.R.ToString();
+            label2.Text = "G: " + clr.G.ToString();
+            label3.Text = "B: " + clr.B.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+
+        {
+            pictureBox2.Image = ConvertToGray(new Bitmap("C:/Users/Sitth/source/repos/WinFormsApp4/WinFormsApp4/im2.png"));
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+    }
+}
